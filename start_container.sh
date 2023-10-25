16c2535d676b458837a55f7a9e4bfde1a6c159d7
AKSHAY K MANOJ <121216014+akshaykmanoj@users.noreply.github.com>
2023-10-25 18:18:58 +0530

    Create start_container.sh

---
+++ start_container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Retrieve parameter values from Parameter Store
+ACCOUNT_ID=$(aws ssm get-parameter --name /myapp/aws-credentials/account_id --query "Parameter.Value" --output text)
+AWS_DEFAULT_REGION=$(aws ssm get-parameter --name /myapp/aws-credentials/region --query "Parameter.Value" --output text)
+REPOSITORY_URI=$(aws ssm get-parameter --name /myapp/aws-credentials/registry --query "Parameter.Value" --output text)
+
+# Authenticate Docker with your ECR registry
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $REPOSITORY_URI
+
+# Pull the image
+docker pull $REPOSITORY_URI/my-image:latest
+
+# Run your container using the pulled image
+docker run -d $REPOSITORY_URI/my-image:latest
